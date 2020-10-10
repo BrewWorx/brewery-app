c223b3f327b8460ebdd944aa421d5ab8d7b1e9ad
Konsta Laakso <konsta.laakso@gmail.com>
2020-10-10 15:37:45 +0300

    Add Router and AppBar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { AppBar, Toolbar, Tabs, Tab } from '@material-ui/core';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <div>
+            <AppBar position="static">
+              <Toolbar>
+                <Tabs>
+                  <Link to="/">
+                    <Tab label="General"></Tab>
+                  </Link>
+                  <Link to="/mash">
+                    <Tab label="Mash & Boil"></Tab>
+                  </Link>
+                  <Link to="/fermentation">
+                    <Tab label="Fermentation"></Tab>
+                  </Link>
+                  <Link to="/digital-twin">
+                    <Tab label="3D View"></Tab>
+                  </Link>
+                </Tabs>
+              </Toolbar>
+            </AppBar>
+          </div>
+        </header>
+        <section>
+          <Switch>
+            <Route path="/mash">
+              <Mash />
+            </Route>
+            <Route path="/fermentation">
+              <Fermentation />
+            </Route>
+            <Route path="/digital-twin">
+              <DigitalTwin />
+            </Route>
+            <Route path="/">
+              <General />
+            </Route>
+          </Switch>
+        </section>
+      </div>
+    </Router>
   );
+
+  function General() {
+    return <h2>General</h2>;
+  }
+
+  function Mash() {
+    return <h2>Mash</h2>;
+  }
+
+  function Fermentation() {
+    return <h2>Fermentation</h2>;
+  }
+
+  function DigitalTwin() {
+    return <h2>DigitalTwin</h2>;
+  }
 }
 
 export default App;
